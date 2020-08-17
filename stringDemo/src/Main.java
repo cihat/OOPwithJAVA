cbe277b425ae134a03d98f7962158d4a1eb855e6
cihat <cihat02020202@gmail.com>
2020-08-18 00:16:47 +0300

    🎈💻✨🎉🎇

--- stringDemo/src/Main.java
+++ stringDemo/src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    String mesaj = "Bugün hava çok güzel.";
+/*    System.out.println("Eleman Sayısı : " + mesaj.length());
+    //Bir stringe ekleme yapar ve yeni bir string yaratır .
+    String newStr = mesaj.concat(" Yaşasın!");
+    System.out.println(newStr);
+    //Stringin içindeki verilen karakterle mi başladığını boolean ile cevap verir .
+    System.out.println(mesaj.startsWith("B"));
+    //Stringin içindeki verilen karakterle mi sonlandığnı  boolean ile cevap verir .
+    System.out.println(mesaj.endsWith("."));
+    //getChars() 4 parametre alıyor .Bir stringi alır ve hedef diziye atulır
+    char[] karaterler = new char[5];
+    mesaj.getChars(0,5, karaterler, 0);
+    System.out.println(karaterler);
+    //String'de ilk bulduğu karaklterin veya karakterlerin index numarasını geri dondürür.
+    System.out.println(mesaj.indexOf('a'));
+    //String'de en solunda aramaya başlar ilk bulduğu karaklterin veya karakterlerin index numarasını geri dondürür.
+    System.out.println(mesaj.lastIndexOf("."));*/
+
+    System.out.println(mesaj.replace(" ", "-"));
+
+    String subString = mesaj.substring(2, 6);
+    System.out.println(subString);
+
+    for (String kelime:mesaj.split(" ") ){
+      System.out.print(kelime + "\n" );
     }
+    System.out.println(mesaj.trim());
+  }
 }
+
+
+
+
+
+
