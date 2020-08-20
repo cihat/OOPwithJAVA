0578c3395a50f9307b404631026f14e5f9fd86c7
cihat <cihat02020202@gmail.com>
2020-08-20 17:43:24 +0300

    selectdemo, SQLExpection

--- JDBC/JdbcIntro/src/Main.java
+++ JDBC/JdbcIntro/src/Main.java
@@ -1,9 +1,34 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
 
   public static void main(String[] args) throws SQLException {
+    Connection connection = null;
+    DbHelper helper = new DbHelper();
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+    try {
+      connection = helper.getConnection();
+      String sql = "insert into world.city (Name,CountryCode,District,Population) values(?,?,?,?)";
+      statement = connection.prepareStatement(sql);
+      statement.setString(1, "Düzce 2");
+      statement.setString(2, "TUR");
+      statement.setString(3, "Turkey");
+      statement.setInt(4, 70000);
+      statement.executeUpdate();
+      System.out.println("Kayıt Eklendi...");
+    } catch (SQLException exception) {
+      helper.showErrorMessage(exception);
+      System.out.println(exception.getMessage());
+    } finally {
+      statement.close();
+      connection.close();
+    }
+  }
+
+  public static void selectDemo() throws SQLException {
     Connection connection = null;
     DbHelper helper = new DbHelper();
     Statement statement = null;
@@ -12,13 +37,20 @@ public class Main {
       connection = helper.getConnection();
       statement = connection.createStatement();
       resultSet = statement.executeQuery("select Code,Name,Continent,Region from country");
+      ArrayList<Country> countries = new ArrayList<Country>();
       while (resultSet.next()) {
-        System.out.println(resultSet.getString("Name"));
+        countries.add(new Country(
+                resultSet.getString("Code"),
+                resultSet.getString("Name"),
+                resultSet.getString("Continent"),
+                resultSet.getString("Region")));
       }
+      System.out.println(countries.size());
+
     } catch (SQLException exception) {
       helper.showErrorMessage(exception);
       System.out.println(exception.getMessage());
-    }finally {
+    } finally {
       connection.close();
     }
   }
