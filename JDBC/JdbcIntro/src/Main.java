f3650481595f38c6765edbfc6236c87e4f23975d
cihat <cihat02020202@gmail.com>
2020-08-20 18:14:54 +0300

    DataBase with MySql of operations

--- JDBC/JdbcIntro/src/Main.java
+++ JDBC/JdbcIntro/src/Main.java
@@ -11,14 +11,12 @@ public class Main {
     ResultSet resultSet = null;
     try {
       connection = helper.getConnection();
-      String sql = "insert into world.city (Name,CountryCode,District,Population) values(?,?,?,?)";
+      String sql = "delete from city where id = 998";
       statement = connection.prepareStatement(sql);
-      statement.setString(1, "Düzce 2");
-      statement.setString(2, "TUR");
-      statement.setString(3, "Turkey");
-      statement.setInt(4, 70000);
-      statement.executeUpdate();
-      System.out.println("Kayıt Eklendi...");
+/*      statement.setString(1, "Turkey");
+      statement.setInt(2, 998);*/
+      int result = statement.executeUpdate();
+      System.out.println("Kayıt Silindi...");
     } catch (SQLException exception) {
       helper.showErrorMessage(exception);
       System.out.println(exception.getMessage());
@@ -51,6 +49,52 @@ public class Main {
       helper.showErrorMessage(exception);
       System.out.println(exception.getMessage());
     } finally {
+      connection.close();
+    }
+  }
+
+  public static void inserData() throws SQLException {
+    Connection connection = null;
+    DbHelper helper = new DbHelper();
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+    try {
+      connection = helper.getConnection();
+      String sql = "insert into world.city (Name,CountryCode,District,Population) values(?,?,?,?)";
+      statement = connection.prepareStatement(sql);
+      statement.setString(1, "Düzce 2");
+      statement.setString(2, "TUR");
+      statement.setString(3, "Turkey");
+      statement.setInt(4, 70000);
+      statement.executeUpdate();
+      System.out.println("Kayıt Eklendi...");
+    } catch (SQLException exception) {
+      helper.showErrorMessage(exception);
+      System.out.println(exception.getMessage());
+    } finally {
+      statement.close();
+      connection.close();
+    }
+  }
+
+  public void updateData() throws SQLException {
+    Connection connection = null;
+    DbHelper helper = new DbHelper();
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+    try {
+      connection = helper.getConnection();
+      String sql = "update city set population='180260',district='Turkey'   where id = 998";
+      statement = connection.prepareStatement(sql);
+/*      statement.setString(1, "Turkey");
+      statement.setInt(2, 998);*/
+      int result = statement.executeUpdate();
+      System.out.println("Kayıt Güncellendi...");
+    } catch (SQLException exception) {
+      helper.showErrorMessage(exception);
+      System.out.println(exception.getMessage());
+    } finally {
+      statement.close();
       connection.close();
     }
   }
